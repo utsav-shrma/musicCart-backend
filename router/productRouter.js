33663819f2ffe216d7978cfbde6dc1c84b79032e
Utsav Sharma <utsav19913@gmail.com>
2024-03-22 23:19:55 +0530

    edited data being sent and add a check

--- router/productRouter.js
+++ router/productRouter.js
@@ -3,18 +3,20 @@ const productRouter = express.Router();
 require("dotenv").config();
 const Product = require("../models/product");
 const authMiddleware = require("../middleware/auth");
+const product = require("../models/product");
 
 productRouter.get("/",async (req,res)=>{
 
     try{
-        let products = await Product.find({},{
+        let response = await Product.find({},{
             name:true,
             price:true,
             color:true,
             category:true,
-            summary:true
+            summary:true,
+            images:true
             });
-        return res.status(200).json(products);
+        return res.status(200).json(response);
 
     }
     catch(error){
@@ -24,10 +26,14 @@ productRouter.get("/",async (req,res)=>{
 
 }); 
 
-productRouter.get("/:id",async (req,res)=>{
+productRouter.get("/id/:id",async (req,res)=>{
     let {id}=req.params;
-
+    
     try{
+        if(!id){
+            console.log("bad request");
+            return res.status(400).json({error:"bad request"});
+        }
         let response = await Product.findOne({_id:id});
         if (!response) {
             return res.status(404).json({ error: "product not found" });
@@ -37,7 +43,9 @@ productRouter.get("/:id",async (req,res)=>{
         return res.status(200).json(newObj);
     }
     catch(error){
+        
         console.log(error);
+        
     }
         
 
