d3ba9828a1770e80f37410cae25da9302838e1a5
Utsav Sharma <utsav19913@gmail.com>
2024-03-23 00:12:04 +0530

    added search and filter to api

--- router/productRouter.js
+++ router/productRouter.js
@@ -4,18 +4,44 @@ require("dotenv").config();
 const Product = require("../models/product");
 const authMiddleware = require("../middleware/auth");
 const product = require("../models/product");
+const {sortKeyMap,priceRangeKeyMap} = require("../utility/uitlityConstants");
+
 
 productRouter.get("/",async (req,res)=>{
 
+    let query={};
+    let sortQuery={};
+    let priceKey = req.query.priceKey;
+    let category = req.query.category;
+    let color= req.query.color;
+    let company= req.query.company;
+    let sortKey=req.query.sortKey;
+
+    if(sortKey){  
+       sortQuery=sortKeyMap[sortKey]; 
+    }
+    if(priceKey){
+        query.price={$gte:priceRangeKeyMap[priceKey].min,$lte:priceRangeKeyMap[priceKey].max}
+    }
+    if(category){
+        query.category=category;
+    }
+    if(color){
+        query.color=color;
+    }
+    if(company){
+        query.company=company;
+    }
+
     try{
-        let response = await Product.find({},{
+        let response = await Product.find(query,{
             name:true,
             price:true,
             color:true,
             category:true,
             summary:true,
             images:true
-            });
+            }).sort(sortQuery);
         return res.status(200).json(response);
 
     }
---
+++ utility/uitlityConstants.js
@@ -0,0 +1,15 @@
+const priceRangeKeyMap={
+    1:{min:2000,max:3000},
+    2:{min:3000,max:4000},
+    3:{min:5000,max:6000},
+    4:{min:6000,max:7000},
+}
+
+const sortKeyMap={
+    1:{price:"asc"},
+    2:{price:"desc"},
+    3:{name:"asc"},
+    4:{name:"desc"},
+}
+
+module.exports ={priceRangeKeyMap,sortKeyMap};
