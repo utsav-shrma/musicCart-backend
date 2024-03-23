10870b6fdfebdd19a81c6e2669ed13c984540607
Utsav Sharma <utsav19913@gmail.com>
2024-03-24 00:08:28 +0530

    added cart apis

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongodb =require('./config/mongodb');
 const authMiddleware =require('./middleware/auth');
 const userRouter=require('./router/userRouter');
 const productRouter=require('./router/productRouter');
-
+const cartRouter=require('./router/cartRouter');
 const app=express();
 const port=process.env.PORT;
 
@@ -13,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 app.use(userRouter);
 app.use('/product',productRouter);
+app.use('/cart',authMiddleware,cartRouter);
 app.use(express.static('assets'))
 
 app.get('/',(req,res)=>{  
---
+++ router/cartRouter.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const cartRouter = express.Router();
+require("dotenv").config();
+const User = require("../models/user");
+
+cartRouter.get('/',async(req,res)=>{
+    
+    try{
+        const userId=req.body.userId;
+        let response= await User.findOne({_id:userId},{cart:true});
+        if(response){
+            return res.status(200).json(response);
+        }
+        
+    }
+    catch(error){
+        console.log(error);
+    }
+
+});
+
+cartRouter.post('/',async(req,res)=>{
+    
+    try{
+
+        const userId=req.body.userId;
+        const {productId,qty}=req.body;
+
+        if(!productId || !qty){
+            return res.status(400).json({ message: "bad request" });
+        }
+
+        let response= await User.findOne({_id:userId});
+        let cart=response.cart;
+        let cartCount=0;
+        let index =-1;
+         cart.find(function (element,i) {
+            if(element.productId==productId){
+                index=i;
+                
+            }
+            
+            cartCount+=element.qty;
+        });
+
+        if(index!=-1){
+            cart[index]={productId,qty:cart[index].qty+qty};
+        }
+        else{  
+            cart.push({productId,qty});
+        }
+        
+
+        await User.updateOne(
+            { _id: userId },
+            { $set: { cart } }
+          );
+
+        cartCount+=qty;
+          return res.status(200).json({message:"success",cartCount});  
+        
+    }
+    catch(error){
+        console.log(error);
+    }
+
+});
+
+cartRouter.get('/count',async(req,res)=>{
+    
+    try{
+        const userId=req.body.userId;
+        let response= await User.findOne({_id:userId},{cart:true});
+        if(response){
+            let count=0;
+            response.cart.map((item)=>{count+=item.qty;});
+            return res.status(200).json({cartCount:count});
+        }
+        else{
+            return res.status(404).json({error:"NOT FOUND"});
+        }
+        
+    }
+    catch(error){
+        console.log(error);
+    }
+
+});
+module.exports = cartRouter;
