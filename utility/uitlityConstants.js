faf4c8aa46d66baaab65b6436c5dab7c8458e56c
Utsav Sharma <utsav19913@gmail.com>
2024-04-01 19:42:45 +0530

    added sample images and updated const

--- utility/uitlityConstants.js
+++ utility/uitlityConstants.js
@@ -1,8 +1,7 @@
 const priceRangeKeyMap={
     1:{min:0,max:1000},
-    2:{min:1000,max:2000},
-    3:{min:2000,max:5000},
-    4:{min:6000,max:7000},
+    2:{min:1000,max:10000},
+    3:{min:10000,max:20000},
 }
 
 const sortKeyMap={
